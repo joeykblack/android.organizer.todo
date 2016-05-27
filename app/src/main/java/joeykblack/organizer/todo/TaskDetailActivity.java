a63ffc96308e82c1411dfd33ce3ea8b80a502ecd
Joey K Black <joeykblack@gmail.com>
2016-05-27 11:09:00 -0400

    Using NumberPicker

--- app/src/main/java/joeykblack/organizer/todo/TaskDetailActivity.java
+++ app/src/main/java/joeykblack/organizer/todo/TaskDetailActivity.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.NumberPicker;
 
 import joeykblack.organizer.todo.database.TaskDbHelper;
 import joeykblack.organizer.todo.fragment.DatePickerFragment;
@@ -30,21 +31,36 @@ public class TaskDetailActivity extends AppCompatActivity {
 
         mHelper = new TaskDbHelper(this);
 
+        // Setup Priority NumberPicker
+        NumberPicker numberPicker = (NumberPicker) findViewById(R.id.edit_task_priority);
+        numberPicker.setMinValue(0);
+        numberPicker.setMaxValue(10);
+        numberPicker.setWrapSelectorWheel(false);
+
+        // Save button
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new SaveOnClickListener(this));
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        // Prepopulate if Editing
         Task task = (Task) getIntent().getSerializableExtra(Task.TAG);
         if ( task != null ) {
             this.task = task;
+
+            // Title
             EditText editTaskTitle = (EditText) this.findViewById(R.id.edit_task_title);
             editTaskTitle.setText(task.getTitle());
-            EditText editTaskPriority = (EditText) this.findViewById(R.id.edit_task_priority);
-            editTaskPriority.setText( String.valueOf( task.getPriority() ) );
+
+            // Priority
+            NumberPicker editTaskPriority = (NumberPicker) this.findViewById(R.id.edit_task_priority);
+            editTaskPriority.setValue( task.getPriority() );
+
+            // Date
             Button editTaskDate = (Button) this.findViewById(R.id.edit_task_date);
             if ( task.getDate() != null ) {
                 editTaskDate.setText( TaskDbHelper.serializeDate( task.getDate() ) );
             }
+
             Log.d(TAG, "Loading: " + task.getTitle() + " priority[" + task.getPriority() + "] date[" + task.getDate() + "]");
         }
     }
--- app/src/main/java/joeykblack/organizer/todo/listeners/SaveOnClickListener.java
+++ app/src/main/java/joeykblack/organizer/todo/listeners/SaveOnClickListener.java
@@ -1,20 +1,18 @@
 package joeykblack.organizer.todo.listeners;
 
 import android.content.ContentValues;
-import android.content.Context;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
-import java.util.Date;
+import android.widget.NumberPicker;
 
 import joeykblack.organizer.todo.QueueActivity;
+import joeykblack.organizer.todo.R;
 import joeykblack.organizer.todo.TaskDetailActivity;
 import joeykblack.organizer.todo.database.TaskContract;
-import joeykblack.organizer.todo.R;
 import joeykblack.organizer.todo.database.TaskDbHelper;
 import joeykblack.organizer.todo.model.Task;
 
@@ -32,25 +30,30 @@ public class SaveOnClickListener implements View.OnClickListener {
     public void onClick(View view) {
         View parent = (View) view.getParent();
 
+        // Get title
         EditText editTaskTitle = (EditText) parent.findViewById(R.id.edit_task_title);
         String title = String.valueOf(editTaskTitle.getText());
 
-        EditText editTaskPriority = (EditText) parent.findViewById(R.id.edit_task_priority);
-        int priority = Integer.valueOf( String.valueOf(editTaskPriority.getText()) );
+        // Get priority
+        NumberPicker editTaskPriority = (NumberPicker) parent.findViewById(R.id.edit_task_priority);
+        int priority = editTaskPriority.getValue();
 
+        // Get date
         Button editTaskDate = (Button) parent.findViewById(R.id.edit_task_date);
         String dateString = String.valueOf(editTaskDate.getText());
         dateString = TaskDbHelper.isDate(dateString) ? dateString : null;
 
-        Task task = myContext.getTask();
-
-        SQLiteDatabase db = mHelper.getWritableDatabase();
+        // Prepare values
         ContentValues values = new ContentValues();
         values.put(TaskContract.TaskEntry.COL_TASK_TITLE, title);
         values.put(TaskContract.TaskEntry.COL_TASK_PRIORITY, priority);
         values.put(TaskContract.TaskEntry.COL_TASK_DATE, dateString);
 
+        SQLiteDatabase db = mHelper.getWritableDatabase();
+        Task task = myContext.getTask();
+
         if ( task == null ) {
+            // Create Task
             Log.d(TAG, "Insert into db: " + title + " priority[" + priority + "] date[" + dateString + "]");
             db.insertWithOnConflict(TaskContract.TaskEntry.TABLE,
                     null,
@@ -58,6 +61,7 @@ public class SaveOnClickListener implements View.OnClickListener {
                     SQLiteDatabase.CONFLICT_REPLACE);
         }
         else {
+            // Update Task
             Log.d(TAG, "Updating into db: " + title + " priority[" + priority + "] date[" + dateString + "]");
             db.update(TaskContract.TaskEntry.TABLE,
                     values,
